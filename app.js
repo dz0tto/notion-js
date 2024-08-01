c101bb49f9e6449e14635a569ade6cdfbaf372fb
dz0tto <dzotto@gmail.com>
2024-08-01 10:27:09 +0200

    try fix port

--- app.js
+++ app.js
@@ -54,8 +54,10 @@ app.post('/webhook/tilda', (req, res) => {
     processTildaReq(req, res);
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
 
